71cb918a3c104df860608cb38a740f746816c92d
wallace14k <wallaceoliveiraaqw15@gmail.com>
2023-03-12 11:49:48 -0300

    Ajustes no banco de dados o mesmo foi migrado para o docker

---
+++ Centaury.Infra/Migrations/20230312132823_Tentativa de criação do banco de dados.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Centaury.Infra.Migrations
+{
+    public partial class Tentativadecriaçãodobancodedados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
